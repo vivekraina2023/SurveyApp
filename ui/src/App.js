10f0b3247dcba19773cee0d325236aa09702d906
vivekraina2023 <vivekraina2023@gmail.com>
2024-12-15 16:59:48 -0500

    base version for UI

---
+++ ui/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import SurveyDesigner from './components/SurveyDesigner';
+
+function App() {
+  return (
+    <div className="App">
+      <SurveyDesigner />
+    </div>
+  );
+  
+}
+
+
+
+export default App;
